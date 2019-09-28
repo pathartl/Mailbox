bca25c308a27c98816b36402bc7c342bcb922d27
Pat Hartl <pat@pathar.tl>
2019-09-28 02:38:16 -0500

    Init database. Handle mailbox write/delete from database.

--- src/tl/pathar/minecraft/mailbox/Mailbox.java
+++ src/tl/pathar/minecraft/mailbox/Mailbox.java
@@ -1,10 +1,23 @@
 package tl.pathar.minecraft.mailbox;
 
+import com.pablo67340.SQLiteLib.Main.SQLiteLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Mailbox extends JavaPlugin {
+    public SQLiteLib sqlLib;
+
     @Override
     public void onEnable() {
+        sqlLib = SQLiteLib.hookSQLiteLib(this);
+
+        sqlLib.initializeDatabase("Mailbox", "CREATE TABLE IF NOT EXISTS \"Mailboxes\" (\n" +
+                "\t\"Id\"\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,\n" +
+                "\t\"Player\"\tTEXT,\n" +
+                "\t\"X\"\tINTEGER,\n" +
+                "\t\"Y\"\tINTEGER,\n" +
+                "\t\"Z\"\tINTEGER\n" +
+                ");");
+
         getCommand("mailbox").setExecutor(new ExampleCommand(this));
         // getServer().getPluginManager().registerEvents(new MailboxListener(this), this);
         getServer().getPluginManager().registerEvents(new MailboxConstructionListener(this), this);
--- src/tl/pathar/minecraft/mailbox/MailboxConstructionListener.java
+++ src/tl/pathar/minecraft/mailbox/MailboxConstructionListener.java
@@ -1,5 +1,6 @@
 package tl.pathar.minecraft.mailbox;
 
+import com.pablo67340.SQLiteLib.Database.Database;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -11,15 +12,18 @@ import org.bukkit.block.data.type.RedstoneWallTorch;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
 public class MailboxConstructionListener implements Listener {
     private Mailbox plugin;
+    private Database database;
 
     public MailboxConstructionListener(Mailbox _plugin) {
         plugin = _plugin;
+        database = plugin.sqlLib.getDatabase("Mailbox");
     }
 
     @EventHandler
@@ -60,7 +64,62 @@ public class MailboxConstructionListener implements Listener {
 
                     redstoneWallTorchBlockData.setLit(false);
                     mailbox.redstoneWallTorch.setBlockData(redstoneWallTorchBlockData);
+
+                    Location barrelLocation = mailbox.barrel.getLocation();
+
+                    database.executeStatement(
+                            String.format(
+                                    "INSERT INTO Mailboxes (\"Player\", \"X\", \"Y\", \"Z\") VALUES (\"%s\", %d, %d, %d)",
+                                    player.getName(),
+                                    barrelLocation.getBlockX(),
+                                    barrelLocation.getBlockY(),
+                                    barrelLocation.getBlockZ()
+                            )
+                    );
                 }
+        }
+    }
+
+    @EventHandler
+    public void onBlockDestroy(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Player player = event.getPlayer();
+
+        switch (block.getType()) {
+            case BARREL:
+            case OAK_FENCE:
+            case REDSTONE_WALL_TORCH:
+                MailboxStructure mailbox = getMailboxStructureFromPart(block);
+
+                if (mailbox.isValidMailbox()) {
+                    Location barrelLocation = mailbox.barrel.getLocation();
+                    String mailboxOwner = (String)database.queryValue(
+                            String.format(
+                                    "SELECT * FROM Mailboxes WHERE X = %d AND Y = %d AND Z = %d",
+                                    barrelLocation.getBlockX(),
+                                    barrelLocation.getBlockY(),
+                                    barrelLocation.getBlockZ()
+                            ),
+                            "Player"
+                    );
+
+                    database.executeStatement(
+                            String.format(
+                                    "DELETE FROM Mailboxes WHERE X = %d AND Y = %d AND Z = %d",
+                                    barrelLocation.getBlockX(),
+                                    barrelLocation.getBlockY(),
+                                    barrelLocation.getBlockZ()
+                            )
+                    );
+
+                    if (!mailboxOwner.equals(player.getName())) {
+                        plugin.getServer().broadcastMessage("Hey, " + player.getName() + " bashed in " + mailboxOwner + "'s mailbox!");
+                    } else {
+                        player.sendMessage("Your mailbox has been destroyed.");
+                    }
+                }
+
+                break;
         }
     }
 
